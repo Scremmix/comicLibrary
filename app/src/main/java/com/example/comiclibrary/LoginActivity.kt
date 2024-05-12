076f3e5dbee96ccf19168dd482904081d57df459
Alessandro Scremin <57624331+Scremmix@users.noreply.github.com>
2024-05-12 16:05:47 +0200

    Modificato il layout della pagina di login affinchè mostri: - l'icona dell'app e il suo nome; - due box per inserire, rispettivamente, indirizzo mail e password; - un bottone per effettuare il login e uno per la registrazione. Creati i metodi per elaborare la richiesta di login e di passaggio alla schermata di registrazione.

--- app/src/main/java/com/example/comiclibrary/LoginActivity.kt
+++ app/src/main/java/com/example/comiclibrary/LoginActivity.kt
@@ -2,10 +2,21 @@ package com.example.comiclibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+    fun loginRun(view: View)
+    {
+        Toast.makeText(this, "Hai premuto sul bottone di login!", Toast.LENGTH_SHORT).show()
+    }
+
+    fun toRegister(view: View)
+    {
+        Toast.makeText(this, "Hai premuto sul bottone di register!", Toast.LENGTH_SHORT).show()
     }
 }
